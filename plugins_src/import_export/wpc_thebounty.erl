ae4c2c0356ef476d634dd4ac5fcbba195aef8a49
micheus <micheus@gmail.com>
2021-05-11 11:07:02 -0300

    It was missed the ending tag of lightportal data in the export section.
    NOTE:
    
    Small fixes to Yafaray plugin. Thanks to oort.

--- plugins_src/import_export/wpc_thebounty.erl
+++ plugins_src/import_export/wpc_thebounty.erl
@@ -4325,8 +4325,6 @@ export_object_1(F, NameStr, Mesh0=#e3d_mesh{he=He0}, DefaultMaterial, MatPs, Id)
                                      if AutosmoothAngle == 0.0 -> false;
                                         true -> ?DEF_AUTOSMOOTH end),
 
-
-
     %% Pre-process mesh
     Mesh1 = #e3d_mesh{} =
         case {He0,UseHardness} of
@@ -4430,6 +4428,8 @@ export_object_1(F, NameStr, Mesh0=#e3d_mesh{he=He0}, DefaultMaterial, MatPs, Id)
             println(F, "<with_diffuse bval=\"~s\"/>",[Lightportal_Diffusephotons]),
             println(F, "<with_caustic bval=\"~s\"/>",[Lightportal_Causticphotons]),
             println(F, "<photon_only bval=\"~s\"/>",[Lightportal_Photon_Only]),
+            println(F, "</light>"),
+            println(F, "<mesh id=\"~w\" vertices=\"~w\" faces=\"~w\" has_uv=\"~s\" type=\"0\">",[Id,length(Vs),length(Fs),HasUV]),
             println(F," ")
 
     end,
@@ -4466,7 +4466,7 @@ export_object_1(F, NameStr, Mesh0=#e3d_mesh{he=He0}, DefaultMaterial, MatPs, Id)
 
         lightportal ->
             println(F," "),
-            println(F, "</light>"),
+            println(F, "</mesh>"),
             println(F," ")
     end,
 
